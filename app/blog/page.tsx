5f28440b3d7e3a45dfe6c76257f20c4c19630d90
Jarren San Jose <sanjose.jarren@gmail.com>
2024-07-28 17:07:46 -0700

    clean up blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,14 +1,14 @@
 import { BlogPosts } from 'app/components/posts'
 
 export const metadata = {
-  title: 'Blog',
-  description: 'Read my blog.',
+  title: 'leftcurver',
+  description: 'leftcurver',
 }
 
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">leftcurvers blog</h1>
       <BlogPosts />
     </section>
   )
