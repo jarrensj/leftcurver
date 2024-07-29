8478a70c8f503388cbf4428c0570f711bb84a5ff
Jarren San Jose <sanjose.jarren@gmail.com>
2024-07-28 17:08:35 -0700

    edit nav bar

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -7,8 +7,11 @@ const navItems = {
   '/blog': {
     name: 'blog',
   },
-  'https://vercel.com/templates/next.js/portfolio-starter-kit': {
-    name: 'deploy',
+  '/team': {
+    name: 'team',
+  },
+  '/portfolio': {
+    name: 'portfolio',
   },
 }
 
