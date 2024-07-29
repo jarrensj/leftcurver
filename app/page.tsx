e91a4c60e5bfd1de8b58543db59e258ed6036187
Jarren San Jose <sanjose.jarren@gmail.com>
2024-07-28 17:08:08 -0700

    add about and disclaimer

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,13 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        leftcurvers
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`We're a group of traders, builders, and thinkers who are passionate and curious. We're not financial advisors. We're not really smart. We're leftcurvers.`}
+      </p>
+      <p>
+        {`Disclaimer: This is not financial advice.`}
       </p>
       <div className="my-8">
         <BlogPosts />
